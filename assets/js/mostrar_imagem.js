a8e42751ba39d4fd7bae740c2385926797415abe
rafa <rafasalvajolileite@gmail.com>
2024-10-19 16:50:45 -0300

    pin - css

---
+++ assets/js/mostrar_imagem.js
@@ -0,0 +1,11 @@
+function previewImage(event) {
+    const file = event.target.files[0];
+    const preview = document.getElementById('image-preview');
+    preview.innerHTML = '';
+    if (file) {
+        const img = document.createElement('img');
+        img.src = URL.createObjectURL(file);
+        img.onload = () => URL.revokeObjectURL(img.src);
+        preview.appendChild('img');
+    }
+}
