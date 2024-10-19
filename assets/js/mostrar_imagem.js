2b43714582e86c9af5b8f4218bac1b2ea01ba5e4
rafa <rafasalvajolileite@gmail.com>
2024-10-19 19:18:45 -0300

    preview-image

--- assets/js/mostrar_imagem.js
+++ assets/js/mostrar_imagem.js
@@ -1,11 +1,15 @@
-function previewImage(event) {
-    const file = event.target.files[0];
-    const preview = document.getElementById('image-preview');
-    preview.innerHTML = '';
-    if (file) {
-        const img = document.createElement('img');
-        img.src = URL.createObjectURL(file);
-        img.onload = () => URL.revokeObjectURL(img.src);
-        preview.appendChild('img');
+$(document).ready(function(){
+    function previewImage(event) {
+        const file = event.target.files[0];
+        const preview = document.getElementById('image-preview');
+        preview.innerHTML = '';
+        if (file) {
+            const img = document.createElement('img');
+            img.src = URL.createObjectURL(file);
+            img.onload = () => URL.revokeObjectURL(img.src);
+            preview.appendChild(img);
+        }
     }
-}
+
+    $('#input-file').on('change', previewImage);
+});
